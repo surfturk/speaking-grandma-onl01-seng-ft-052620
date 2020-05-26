08ca8bc3653823552d20acc01ccb795af85178f6
Berkley Brown <berkleydeane@gmail.com>
2020-05-26 22:59:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,6 @@ def speak_to_grandma(string)
     "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     "NO, NOT SINCE 1938!"
-  end
-  
-else 
+  else 
   "HUH?! SPEAK UP, SONNY!"
 end  
